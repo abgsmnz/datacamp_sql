3df3f16a11b2200114a2b97082ae8c795aad98ce
abgsmnz <108446926+abgsmnz@users.noreply.github.com>
2022-06-30 01:01:05 -0400

    Adding a second part

--- select-into.sql
+++ select-into.sql
@@ -1,3 +1,5 @@
+-- ############ Part 1 ############
+
 SELECT country_code, size,
     CASE WHEN size > 50000000 THEN 'large'
         WHEN size > 1000000 THEN 'medium'
@@ -11,3 +13,27 @@ WHERE year = 2015;
 -- Select all columns of pop_plus
 SELECT *
 FROM pop_plus;
+
+-- ############ Part 2 ############
+
+SELECT country_code, size,
+  CASE WHEN size > 50000000
+            THEN 'large'
+       WHEN size > 1000000
+            THEN 'medium'
+       ELSE 'small' END
+       AS popsize_group
+INTO pop_plus       
+FROM populations
+WHERE year = 2015;
+
+-- Select fields
+SELECT name, continent, geosize_group, popsize_group
+-- From countries_plus (alias as c)
+FROM countries_plus AS c
+  -- Join to pop_plus (alias as p)
+  INNER JOIN pop_plus AS p
+    -- Match on country code
+    ON c.code = p.country_code
+-- Order the table    
+ORDER BY geosize_group;
