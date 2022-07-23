64d042fae0a68dd635a3b5382ed22b890bf24d98
Andrea <108446926+abgsmnz@users.noreply.github.com>
2022-07-23 03:16:19 -0400

    Create cross-join.sql

---
+++ cross-join.sql
@@ -0,0 +1,10 @@
+-- CROSS JOIN --
+-- Select fields
+SELECT c.name AS city, l.name AS language
+-- From cities (alias as c)
+FROM cities AS c        
+  -- Join to languages (alias as l)
+  CROSS JOIN languages AS l
+-- Where c.name like Hyderabad
+WHERE c.name LIKE 'Hyder%';
+
