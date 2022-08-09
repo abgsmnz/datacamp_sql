64466c0d18e7fed782b1423ee4749dccfdfe7a49
Andrea <108446926+abgsmnz@users.noreply.github.com>
2022-08-09 00:22:47 -0400

    Update subqueries.sql

--- subqueries.sql
+++ subqueries.sql
@@ -18,3 +18,70 @@ WHERE life_expectancy >
    FROM populations
   WHERE year = 2015)
   AND year = 2015 ;
+
+-- Excersise 2--
+-- Select fields
+SELECT name, country_code, urbanarea_pop
+  -- From cities
+  FROM cities
+-- Where city name in the field of capital cities
+WHERE name IN
+  -- Subquery
+  (SELECT capital
+   FROM countries)
+ORDER BY urbanarea_pop DESC;
+
+-Transforming codes-
+/* 
+SELECT countries.name AS country, COUNT(*) AS cities_num
+  FROM cities
+    INNER JOIN countries
+    ON countries.code = cities.country_code
+GROUP BY country
+ORDER BY cities_num DESC, country
+LIMIT 9;
+*/
+ 
+SELECT name AS country,
+  (SELECT COUNT(cities)
+   FROM cities
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num DESC, country
+LIMIT 9;
+
+--same same, different different
+
+/*
+SELECT countries.name AS country, COUNT(*) AS cities_num
+  FROM cities
+    INNER JOIN countries
+    ON countries.code = cities.country_code
+GROUP BY country
+ORDER BY cities_num DESC, country
+LIMIT 9;
+*/
+
+SELECT name AS country,
+  -- Subquery
+  (SELECT COUNT(name)
+   FROM cities
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num DESC, country
+LIMIT 9;
+
+-- Subquery inside from --
+
+-- Select fields
+SELECT local_name, subquery.lang_num
+  -- From countries
+  FROM countries,
+  	-- Subquery (alias as subquery)
+  	(SELECT code, COUNT(*) AS lang_num
+  	 FROM languages
+  	 GROUP BY code) AS subquery
+  -- Where codes match
+  WHERE countries.code = subquery.code
+-- Order by descending number of languages
+ORDER BY lang_num DESC;
