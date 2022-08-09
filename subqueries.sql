4a9d9ac741cdce6002adb9388aa04d60bf6e8881
Andrea <108446926+abgsmnz@users.noreply.github.com>
2022-08-09 01:47:24 -0400

    Update subqueries.sql

--- subqueries.sql
+++ subqueries.sql
@@ -85,3 +85,66 @@ SELECT local_name, subquery.lang_num
   WHERE countries.code = subquery.code
 -- Order by descending number of languages
 ORDER BY lang_num DESC;
+
+-- Advanced subquery step by step --
+--Step 1: with Inner Join--
+
+-- Select fields
+SELECT name, continent, inflation_rate
+  -- From countries
+  FROM countries
+  	-- Join to economies
+  	INNER JOIN economies
+    -- Match on code
+    USING (code)
+-- Where year is 2015
+WHERE year = 2015;
+
+--Step 2: selecting max inflation rate--
+-- Select the maximum inflation rate as max_inf
+SELECT MAX (inflation_rate) AS max_inf
+  -- Subquery using FROM (alias as subquery)
+  FROM (
+      SELECT name, continent, inflation_rate
+      FROM countries
+      INNER JOIN economies
+      USING (code)
+      WHERE year = 2015) AS subquery
+-- Group by continent
+GROUP BY continent;
+
+--Step 3: Advanced advanced!
+-- Select fields
+SELECT name, continent, inflation_rate
+  -- From countries
+  FROM countries
+	-- Join to economies
+	INNER JOIN economies
+	-- Match on code
+	ON countries.code = economies.code
+  -- Where year is 2015
+  WHERE year = 2015
+    -- And inflation rate in subquery (alias as subquery)
+    AND economies.inflation_rate IN (
+        SELECT MAX(inflation_rate) AS max_inf
+        FROM (
+             SELECT name, continent, inflation_rate
+             FROM countries
+             INNER JOIN economies
+             ON countries.code = economies.code
+             WHERE year = 2015) AS subquery
+      -- Group by continent
+        GROUP BY continent);
+        
+        -- Subquery Challenge --
+  SELECT code, inflation_rate, unemployment_rate
+FROM economies
+WHERE year = 2015 AND economies.code NOT IN ( 
+  SELECT code 
+  FROM countries
+  WHERE (
+    gov_form = 'Constitutional Monarchy' OR 
+    gov_form LIKE '%Republic%'
+  )
+)
+ORDER BY inflation_rate;
