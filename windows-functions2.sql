a000bcfd23441c5604c1a861f706d18c717c78a3
Andrea <108446926+abgsmnz@users.noreply.github.com>
2022-12-12 14:00:49 -0300

    Update windows-functions2.sql

--- windows-functions2.sql
+++ windows-functions2.sql
@@ -248,4 +248,116 @@ ORDER BY Year ASC;
 
 
 
+/*Moving maximum of Scandinavian athletes' medals
+Frames allow you to restrict the rows passed as input to your window function to a sliding window for you to define the start and finish.
+Adding a frame to your window function allows you to calculate "moving" metrics, inputs of which slide from row to row.*/
+
+-- Return the year, medals earned, and the maximum medals earned, comparing only the current year and the next year.
+
+WITH Scandinavian_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('DEN', 'NOR', 'FIN', 'SWE', 'ISL')
+    AND Medal = 'Gold'
+  GROUP BY Year)
+
+SELECT
+  -- Select each year's medals
+  year,
+  medals,
+  -- Get the max of the current and next years'  medals
+  MAX(medals) OVER (ORDER BY year ASC
+             ROWS BETWEEN CURRENT ROW
+             AND 1 FOLLOWING) AS Max_Medals
+FROM Scandinavian_Medals
+ORDER BY Year ASC;
+
+
+
+/*Moving maximum of Chinese athletes' medals
+Frames allow you to "peek" forwards or backward without first using the relative fetching functions, 
+LAG and LEAD, to fetch previous rows' values into the current row.*/
+
+-- Return the athletes, medals earned, and the maximum medals earned, comparing only the last two and current athletes, 
+-- ordering by athletes' names in alphabetical order.
+
+WITH Chinese_Medals AS (
+  SELECT
+    Athlete, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'CHN' AND Medal = 'Gold'
+    AND Year >= 2000
+  GROUP BY Athlete)
+
+SELECT
+  -- Select the athletes and the medals they've earned
+  Athlete,
+  Medals,
+  -- Get the max of the last two and current rows' medals 
+  MAX(Medals) OVER (ORDER BY Athlete ASC
+            ROWS BETWEEN 2 PRECEDING
+            AND CURRENT ROW) AS Max_Medals
+FROM Chinese_Medals
+ORDER BY Athlete ASC;
+
+
+/*Moving average of Russian medals
+Using frames with aggregate window functions allow you to calculate many common metrics, including moving averages and totals. 
+These metrics track the change in performance over time.*/
+
+-- Calculate the 3-year moving average of medals earned.
+
+WITH Russian_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'RUS'
+    AND Medal = 'Gold'
+    AND Year >= 1980
+  GROUP BY Year)
+
+SELECT
+  Year, Medals,
+  --- Calculate the 3-year moving average of medals earned
+  AVG(Medals) OVER
+    (ORDER BY Year ASC
+     ROWS BETWEEN
+     2 PRECEDING AND CURRENT ROW) AS Medals_MA
+FROM Russian_Medals
+ORDER BY Year ASC;
+
+
+/*Moving total of countries' medals
+What if your data is split into multiple groups spread over one or more columns in the table? 
+Even with a defined frame, if you can't somehow separate the groups' data, one group's values will affect the average of another group's values.*/
+
+-- Calculate the 3-year moving sum of medals earned per country.
+
+WITH Country_Medals AS (
+  SELECT
+    Year, Country, COUNT(*) AS Medals
+  FROM Summer_Medals
+  GROUP BY Year, Country)
+
+SELECT
+  Year, Country, Medals,
+  -- Calculate each country's 3-game moving total
+  SUM(Medals) OVER
+    (PARTITION BY Country
+     ORDER BY Year ASC
+     ROWS BETWEEN
+     2 PRECEDING AND CURRENT ROW) AS Medals_MA
+FROM Country_Medals
+ORDER BY Country ASC, Year ASC;
+
+
+
+
+
+
+
 
